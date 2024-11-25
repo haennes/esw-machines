fa48ed2f443aaf78a20eea0aa16e03ecfafc66e8
haennes <hannes.hofmuth@gmail.com>
2024-11-25 10:13:28 +0100

    improve systemd service

--- nix/esw-machines.nix
+++ nix/esw-machines.nix
@@ -3,6 +3,38 @@ let
   inherit (lib) mkEnableOption mkOption getExe;
   inherit (lib.types) port str package path;
   cfg = config.services.esw-machines;
+  dataFileDir = builtins.dirOf cfg.dataFilePath;
+  defaultServiceConfig = {
+    #ReadWritePaths = "${cfg.dataFilePath} ${cfg.package}";
+    #DeviceAllow = "";
+    #LockPersonality = true;
+    #NoNewPrivileges = true;
+    #PrivateDevices = true;
+    #PrivateTmp = true;
+    #PrivateUsers = true;
+    #ProcSubset = "pid";
+    #ProtectClock = true;
+    #ProtectControlGroups = true;
+    #ProtectHome = true;
+    #ProtectHostname = true;
+    #ProtectKernelLogs = true;
+    #ProtectKernelModules = true;
+    #ProtectKernelTunables = true;
+    #ProtectProc = "invisible";
+    #ProtectSystem = "strict";
+    #RemoveIPC = true;
+    #RestrictNamespaces = true;
+    #RestrictRealtime = true;
+    #RestrictSUIDSGID = true;
+    #SystemCallArchitectures = "native";
+    #SystemCallFilter = [ "@system-service" "~@resources" "~@privileged" ];
+    #UMask = "0007";
+    Type = "oneshot";
+    User = cfg.user;
+    Group = config.users.users.${cfg.user}.group;
+    WorkingDirectory = "${cfg.package}/bin";
+    AmbientCapabilities = "CAP_NET_BIND_SERVICE";
+  };
 in {
   options.services.esw-machines = {
     enable = mkEnableOption "esw-machines";
@@ -28,10 +60,12 @@ in {
     users.groups."${cfg.user}" = lib.mkDefault { };
 
     systemd.services.esw-machines = {
+      restartIfChanged = true;
       wantedBy = [ "multi-user.target" ];
+      serviceConfig = defaultServiceConfig;
       script = ''
         pwd
-        mkdir -p ${builtins.dirOf cfg.dataFilePath}
+        mkdir -p ${dataFileDir}
         touch  ${cfg.dataFilePath}
         ${cfg.package.outPath}/bin/esw-machines
       '';
@@ -39,15 +73,12 @@ in {
       environment = {
         LEPTOS_SITE_ADDR = "${cfg.domain}:${toString cfg.port}";
         LEPTOS_OUTPUT_NAME = leptos-options.output-name;
-        #LEPTOS_SITE_ROOT = "${cfg.package.outPath}/bin/site";
         LEPTOS_SITE_ROOT = "site";
-        #LEPTOS_SITE_PKG_DIR = "${cfg.package.outPath}/bin/site/pkg";
         LEPTOS_SITE_PKG_DIR = "pkg";
         LEPTOS_ENV = "DEV"; # TODO check if this should be enabled
         LEPTOS_DB_FILE = cfg.dataFilePath;
 
       };
-      serviceConfig.WorkingDirectory = "${cfg.package}/bin";
     };
   };
 }
