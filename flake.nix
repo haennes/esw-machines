b6cd6ff7af7f0b29b510e4cfb3117318e159dfb1
haennes <hannes.hofmuth@gmail.com>
2026-02-26 23:16:32 +0100

    update

--- flake.nix
+++ flake.nix
@@ -1,19 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=b3582c75c7f21ce0b429898980eddbbf05c68e55";
+    nixpkgs.url = "github:nixos/nixpkgs";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
